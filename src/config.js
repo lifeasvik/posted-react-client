ed330474404605487840c55127fe7fd4b1ccd436
Vik <lifeasvik@gmail.com>
2020-05-08 11:44:17 -0500

    updates now scripts

--- src/config.js
+++ src/config.js
@@ -3,3 +3,5 @@ export default {
   TOKEN_KEY: "local-dev-postcards",
   API_KEY: process.env.REACT_APP_API_KEY,
 };
+// deploy the BE to heroku
+// grab heroku URL and put up on line 2 make sure you have /api
