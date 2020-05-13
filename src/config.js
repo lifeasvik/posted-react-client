e48ace7f79938a1e74744656778b5620cd875113
Vik <lifeasvik@gmail.com>
2020-05-13 16:18:15 -0500

    using heroku url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: "http://localhost:8000/api",
+  API_ENDPOINT: "https://infinite-meadow-33141.herokuapp.com/api",
   TOKEN_KEY: "local-dev-postcards",
   API_KEY: process.env.REACT_APP_API_KEY,
 };
